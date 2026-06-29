fb27c38fff778e759a4b2a88debec51b4da5dbae
vmdeepika <vvmdeepika@gmail.com>
2026-06-29 18:42:31 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Scanner;
+class Main{
+public static void main(String[] args)
+{
+Scanner scan = new Scanner(System.in);
+int age = scan.nextInt();
+String nam =scan.next();
+char ch = scan.next().charAt(0);
+System.out.println(age);
+System.out.println(nam);
+System.out.println(ch);
+}
+}
