2597547065e74efb0b9e001c851678d103de5ade
vmdeepika <vvmdeepika@gmail.com>
2026-06-29 19:55:17 +0530

    Add files via upload

---
+++ code.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+class code{
+  public static void main(String[] args){
+    Scanner scan = new Scanner(System.in);
+    int x = scan.nextInt();
+    int y = scan.nextInt();
+    for (int i=0;i<=y;i++ ){
+      System.out.println(x);
+    } 
+  }
+}
