b9ada65cdac0627aed385fd2c51ae2856720e794
vmdeepika <vvmdeepika@gmail.com>
2026-07-02 17:44:47 +0530

    Add files via upload

---
+++ Pattern (1).java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Pattern {
+    public static void main(String[] args) {
+      System.out.print("Enter the number:");
+      Scanner sc = new Scanner(System.in);
+      int n =sc.nextInt();
+      for(int i=1;i<=n;i++){
+        for(int j=1;j<=i;j++){
+          System.out.print("*");
+        }
+        System.out.println(" ");
+      }
+    }
+}
